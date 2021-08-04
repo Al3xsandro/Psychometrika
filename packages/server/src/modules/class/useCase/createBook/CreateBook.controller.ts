62d276928d47324df853fb6b74c9912f09f9a40d
Alex <alexs123655@gmail.com>
2021-08-04 08:02:12 -0300

    feat: createBook

--- packages/server/src/modules/class/useCase/createBook/CreateBook.controller.ts
+++ packages/server/src/modules/class/useCase/createBook/CreateBook.controller.ts
@@ -1,3 +1,21 @@
+import { Request, Response } from 'express';
 
+import { IBook } from '../../dtos/IBook.dto';
+import { CreateBookCase } from './CreateBookCase';
 
-class CreateBookController {};
+class CreateBookController {
+    async handle(req: Request, res: Response) {
+        const { 
+            title,
+            class_id
+        }:IBook = req.body;
+        
+        const createBookCase = new CreateBookCase();
+
+        const createBook = await createBookCase.execute({ title, class_id });
+
+        return res.json(createBook)
+    }
+};
+
+export { CreateBookController };
---
+++ packages/server/src/modules/class/useCase/createBook/CreateBookCase.ts
@@ -0,0 +1,28 @@
+import { AppError } from "../../../../shared/errors/AppError";
+
+import { IBook } from "../../dtos/IBook.dto";
+import { Book } from "../../schema/Book.schema";
+
+class CreateBookCase {
+    async execute({ title, class_id }: IBook) {
+        if(!title || !class_id){
+            throw new AppError('Invalid title or class_id');
+        };
+        
+        const bookAlreadyExists = await Book.findOne({ title });
+
+        if(bookAlreadyExists){
+            throw new AppError('This book already exists');
+        };
+
+        const createBook = await Book.create({ title, class: class_id });
+    
+        if(!createBook){
+            throw new AppError('An error has occurred', 500);
+        };
+
+        return createBook;
+    };
+};
+
+export { CreateBookCase };
