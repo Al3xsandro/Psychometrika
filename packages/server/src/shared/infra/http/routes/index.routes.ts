30f6c1d4c30dd051e7daf46f080ff59674e5a980
Alex <alexs123655@gmail.com>
2021-08-04 08:22:41 -0300

    feat: new routes

--- packages/server/src/shared/infra/http/routes/index.routes.ts
+++ packages/server/src/shared/infra/http/routes/index.routes.ts
@@ -6,20 +6,29 @@ import {
 
 const index = Router();
 
+import { ensureAdmin } from "../../../../modules/users/middlewares/ensureAdmin";
+import auth from "../../../../modules/users/middlewares/auth";
+
 import { CreateUserController } from "../../../../modules/users/useCases/createUser/CreateUser.controller";
 import { AuthenticateUserController } from "../../../../modules/users/useCases/authenticateUser/AuthenticateUser.controller";
 
 import { CreateClassController } from "../../../../modules/class/useCase/createClass/CreateClass.controller";
+import { CreateBookController } from "../../../../modules/class/useCase/createBook/CreateBook.controller";
+import { CreateChapterController } from "../../../../modules/class/useCase/createChapter/CreateChapter.controller";
 
 const createClassController = new CreateClassController();
+const createBookController = new CreateBookController();
 
 const authenticateUserController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
+const createChapterController = new CreateChapterController();
 
 index.post('/users', createUserController.handle);
 index.post('/login', authenticateUserController.handle);
 
-index.post('/class', createClassController.handle);
+index.post('/class', auth, ensureAdmin, createClassController.handle);
+index.post('/book', auth, ensureAdmin, createBookController.handle);
+index.post('/chapter', auth, ensureAdmin, createChapterController.handle);
 
 index.get('/', (req: Request, res: Response) => {
     res.json({
