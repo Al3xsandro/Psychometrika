169f364559a802722d4474fcf948e6bf2a66a5b8
Alex <alexs123655@gmail.com>
2021-08-05 09:27:01 -0300

    feat: setup sass and styles

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app'
 
+import '../styles/global.scss'
+import styles from '../styles/app.module.scss'; 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      
-    </>
+    <div className={styles.content}>
+
+    </div>
   )
 }
 
