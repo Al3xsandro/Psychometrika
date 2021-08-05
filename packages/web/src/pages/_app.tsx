7ceaf39b19382c5539738242f83f969b54a916ba
Alex <alexs123655@gmail.com>
2021-08-05 16:05:16 -0300

    feat: login page style created

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -2,11 +2,12 @@ import type { AppProps } from 'next/app'
 
 import '../styles/global.scss'
 import styles from '../styles/app.module.scss'; 
+import Home from './index';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={styles.content}>
-
+      <Home />
     </div>
   )
 }
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,9 +1,58 @@
+import { useState } from 'react';
 import Head from 'next/head'
+import { BiHide } from 'react-icons/bi';
+
+import styles from './home.module.scss';
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLogin() {
+
+  };
+
   return (
-    <>
-      
-    </>
+    <div className={styles.container}>
+      <Head>
+        <title>Psychometrika | login</title>
+      </Head>
+
+      <div className={styles.form}>
+        <div className={styles.title}>
+          <img className={styles.logo} src="/logo.png" alt="Psychometrika" />
+          <p className={styles.subtitle}>Desafio Trainee</p>
+        </div>
+
+        <form className={styles.content_input} onSubmit={handleLogin}>
+            
+            <label className={styles.label}>Email</label>
+            
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="Seu email institucional"
+              onChange={event => setEmail(event.target.value)}
+              required
+            />
+
+            <label className={styles.label}>Senha</label>
+            
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Minimo de 8 characteres"
+              onChange={event => setPassword(event.target.value)}
+              required
+            />
+
+            <i className={styles.hideIcon}><BiHide /></i>
+
+            <br/>
+            <button className={styles.button} type="submit">Entrar</button>
+
+        </form>
+      </div>
+    </div>
   );
 };
