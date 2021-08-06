2c34930d7101d30d1a0e02f8afd168bd2e675186
Alex <alexs123655@gmail.com>
2021-08-06 19:57:18 -0300

    fix: responsivite

--- packages/web/src/components/header/index.tsx
+++ packages/web/src/components/header/index.tsx
@@ -10,12 +10,12 @@ export default function Header() {
     return (
         <div className={styles.container}>
             <div className={styles.admin}>
-                <img className={styles.logo} src="/logo.svg" alt="Psychometrika" />
-                <p className={styles.subtitle}>Desafio Trainee</p>
-
-                <img className={styles.avatar} src="/avatar.svg" alt="avatar"/>
+                <div className={styles.title}>
+                    <img className={styles.logo} src="/logo.svg" alt="Psychometrika" />
+                    <p className={styles.subtitle}>Desafio Trainee</p>
+                </div>
                 
-                <a className={styles.admin_modal} onClick={handleClick}><FiChevronDown /></a>
+                <a className={styles.admin_modal} onClick={handleClick}><img className={styles.avatar} src="/avatar.svg" alt="avatar"/> <FiChevronDown /></a>
             </div>
             <div className={styles.section}>
                 <h4 className={styles.title}>Nome da escola</h4>
