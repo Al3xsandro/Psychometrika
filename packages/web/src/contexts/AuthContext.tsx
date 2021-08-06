efd2ed437f7e6264fef04aad2e8e1d1bf998fc93
Alex <alexs123655@gmail.com>
2021-08-06 10:49:10 -0300

    initial commit

---
+++ packages/web/src/contexts/AuthContext.tsx
@@ -0,0 +1,69 @@
+import { 
+    createContext,
+    useState,
+    ReactNode,
+} from 'react';
+
+import Router from 'next/router';
+
+import { api } from '../services/api';
+
+type signIn = {
+    email: string;
+    password: string;
+}
+
+interface AuthContextData {
+    isAuthenticated: boolean;
+    signIn({ email, password }: signIn): Promise<void>;
+};
+
+type AuthContextProviderProps = {
+    children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+    const [isAuthenticated, setAuthenticated] = useState(false);
+    const [isAdmin, setIsAmin] = useState(false);
+
+    async function signIn({
+        email,
+        password
+    }: signIn) {
+        const options = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+        
+        await api.post('/v1/login', {
+            email,
+            password
+        }, options)
+        
+        .then(response => {
+            const data = response.data.token;
+
+            localStorage.setItem('token', data);
+
+            api.defaults.headers[`Authorization`] = `Bearer ${data}`;
+
+            setAuthenticated(true);
+
+            Router.push('/dashboard');
+        }).catch(err => {
+            console.log(err);
+            alert(err);
+            Router.push('/');
+        });
+    };
+
+    return (
+        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+            { children }
+        </AuthContext.Provider>
+    )
+};
