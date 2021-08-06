85c628e4dbf93bc8555930c4f050abecbe71808a
Alex <alexs123655@gmail.com>
2021-08-06 15:55:33 -0300

    feat: header button

--- packages/web/src/components/header/index.tsx
+++ packages/web/src/components/header/index.tsx
@@ -1,6 +1,12 @@
 import styles from './header.module.scss'
 
+import { FiChevronDown } from 'react-icons/fi';
+
 export default function Header() {
+    function handleClick(){
+        return
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.admin}>
@@ -8,6 +14,8 @@ export default function Header() {
                 <p className={styles.subtitle}>Desafio Trainee</p>
 
                 <img className={styles.avatar} src="/avatar.svg" alt="avatar"/>
+                
+                <a className={styles.admin_modal} onClick={handleClick}><FiChevronDown /></a>
             </div>
             <div className={styles.section}>
                 <h4 className={styles.title}>Nome da escola</h4>
