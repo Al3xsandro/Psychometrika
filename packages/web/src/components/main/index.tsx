833c256d5938c7a3f8b90a8c3008e1cfaa2ca08d
Alex <alexs123655@gmail.com>
2021-08-06 22:54:30 -0300

    fix: invalid section removed

--- packages/web/src/components/main/index.tsx
+++ packages/web/src/components/main/index.tsx
@@ -17,15 +17,15 @@ export function Main() {
                 </div>
             </div>
             
-            <div className={styles.section_left}>
-                <div className={styles.section_class_left}>
-                    <h2 className={styles.title}>2 Série</h2>
+            <div className={styles.section_right}>
+                <div className={styles.section_class_right}>
+                    <h2 className={styles.title}>1 Série</h2>
 
-                    <a className={styles.refresh_left}><img src="/refresh.svg" alt="logo" /></a>
+                    <div className={styles.refresh_right}><img src="/refresh.svg" alt="logo" /></div>
                 </div>
-
-                <div className={styles.content_left}>
-                    <div className={styles.title_left}>
+                
+                <div className={styles.content}>
+                    <div className={styles.title}>
                         <h4>Frente A <i style={{ padding: '0.3rem'}}><img src="/edit-icon.svg" alt="icon"/></i></h4>
                     </div>
                 </div>
